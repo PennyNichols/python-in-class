82627007c6696a3ccacde938d2fd25a2ba6c64d5
pnichols789 <pnichols789@gmail.com>
2022-12-08 19:05:12 -0600

    -

---
+++ in-class4/in-class4.py
@@ -0,0 +1,33 @@
+# number1 = float(input('Number1 :'))
+# number2 = float(input('Number2 :'))
+# number3 = float(input('Number3 :'))
+
+# numbers = [number1, number2, number3]
+# largest = number1
+# for i in numbers:
+#     if i > largest:
+#         largest = i
+# print("The largest number is {}".format(largest))
+
+# age = input('please enter a number ')
+
+# while not age.isdigit():
+#     age = input('please enter a valid number')
+# else:
+#     print(int(age))
+
+sentence = input("Please enter a sentence : ")
+
+word_list = sentence.split()
+count1 = len(word_list)
+count2 = 0
+longest = word_list[0]
+
+while count1 > 0:
+    if len(word_list[count2]) > len(longest[0]):
+        longest = word_list[count2]
+    count1 -= 1
+    count2 += 1
+
+print('the longest word in "{}" has {} letters.'.format(sentence, len(longest)))
+print(type(len(longest)))
